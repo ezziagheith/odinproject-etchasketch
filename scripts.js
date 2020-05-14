d1dd474daae2500ae50208b836ae43038f56feca
ezziagheith <ezziagheith@gmail.com>
2020-05-13 17:50:33 -0700

    Etch

---
+++ scripts.js
@@ -0,0 +1,29 @@
+createGrid = (x) => {
+    for (let rows = 0; rows < x; rows++) {
+        for (let columns = 0; columns < x; columns++){
+            $("#container").append("<div class='grid'></div>")
+        }
+    }
+    $(".grid").width(960/x);
+    $(".grid").height(960/x);
+}
+
+
+$(document).ready(function() {
+    createGrid(16);
+
+    $(".grid").mouseover(function(){
+        $(this).css("background-color", "pink");
+    })
+
+    $(".newGrid").click(function() {
+        refreshGrid();
+
+        $(".grid").mouseover(function() {
+            $(this).css("background-color", "pink");
+        })
+    })
+})
+
+
+
